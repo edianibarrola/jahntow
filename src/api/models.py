ad32b21fe5eb106e6fd8771af8fe007184afd7b5
edianibarrola <edianibarrola@gmail.com>
2023-08-08 23:09:15 +0000

    created player model in db

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,41 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    level = db.Column(db.Integer, default=1)
+    experience = db.Column(db.Integer, default=0)
+    health = db.Column(db.Integer, default=100)
+    energy = db.Column(db.Integer, default=100)
+    credits = db.Column(db.Integer, default=1000)
+    equipment = db.Column(db.JSON, default=dict)
+    inventory = db.Column(db.JSON, default=dict)
+    properties = db.Column(db.JSON, default=dict)
+    maxInventoryCount = db.Column(db.Integer, default=10)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # linking the player to a user
+    
+    User.player = db.relationship("Player", uselist=False, back_populates="user")
+
+    # Relationship to link a User to a Player
+    user = db.relationship("User", back_populates="player")
+
+    def __repr__(self):
+        return f'<Player {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "level": self.level,
+            "experience": self.experience,
+            "health": self.health,
+            "energy": self.energy,
+            "credits": self.credits,
+            "equipment": self.equipment,
+            "inventory": self.inventory,
+            "properties": self.properties,
+            "maxInventoryCount": self.maxInventoryCount
+        }
+
