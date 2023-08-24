60e631e7bcaa5f2010addb4a454d66edb324355e
edianibarrola <edianibarrola@gmail.com>
2023-08-24 00:55:51 +0000

    updated story missions

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -998,7 +998,7 @@ const storyMissionsData = {
 
     requiredMissionWins: 0,
   },
-  "Cultural Espionage": {
+  "Protect the Caravans": {
     species: "Aelithians",
     "Required Credits": 800,
     "Required Energy": 8,
@@ -1474,46 +1474,136 @@ const storyMissionsData = {
     requiredMissionWins: 130,
   },
 };
+
 const storyMissionArc = {
   "Mission 0": {
     Characters: ["echo"],
-    Title: "story mission arc title.",
+    Title: "Disable Spy Drones 1",
     Message:
-      "story 0. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space.",
+      "Walking through to Xictilian city, ECHO notices a flock of Spy Drones overhead. Jahntow needs to disable one and show it to the Xictilians",
 
     requiredMissionWins: 0,
   },
   "Mission 1": {
-    Characters: ["echo", "zuark"],
-    Title: "story mission arc title.",
+    Characters: ["zuark"],
+    Title: "Disable Spy Drones 2",
     Message:
-      "story 1. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space.",
+      "Jahntow arrives to the city and is greated by his long time friend Zu-Ark. He informs her of the drones and shows her the chip he extracted from the one he took down. They agree to go out and see if there are any more still circling around.",
 
     requiredMissionWins: 1,
   },
   "Mission 2": {
-    Characters: ["echo", "zuark", "ava"],
-    Title: "story mission arc title.",
+    Characters: ["zuark"],
+    Title: "Disable Spy Drones 3",
     Message:
-      "story 2. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space.",
+      "After taking clearing the skies, a new batch of Spy Drones was reported by the locals. Jahntow and Zuark need to take care of these strays.",
 
     requiredMissionWins: 2,
   },
   "Mission 3": {
-    Characters: ["echo", "zuark", "ava", "robot"],
-    Title: "story mission arc title.",
+    Characters: ["echo", "zuark"],
+    Title: "Disable Spy Drones 4",
     Message:
-      "story 3. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space.",
+      "Jahntow and Zu'ark are almost able to track the location of the drop-ship. they need a few more electronics from the Spy Drones. Go take down more. ",
 
     requiredMissionWins: 3,
   },
   "Mission 4": {
-    Characters: ["echo", "zuark"],
-    Title: "story mission arc title.",
+    Characters: ["echo", "zuark", "robot"],
+    Title: "Disable Spy Drones 5",
     Message:
-      "story 4. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space. ECHO: Elevate your vigilance, Jahntow. Rogue drones sent by the Corporation threaten the harmony of Aelithian space.",
+      "Jahntow and Zu'ark are able to pinpoint the location of the drone drop-ship. Lead a group of warriors to shut it down completely.",
 
     requiredMissionWins: 4,
+  },
+
+  "Mission 5": {
+    Characters: ["veran"],
+    Title: "Protect Caravans 1",
+    Message:
+      "The Spy Drones have been dealt with. Veran, the brother of Zu'Ark came across a destroyed caravan. go investigate with him.",
+
+    requiredMissionWins: 5,
+  },
+
+  "Mission 6": {
+    Characters: ["veran", "echo"],
+    Title: "Protect Caravans 2",
+    Message:
+      "ECHO analyzes the damaged caravan. Vortex Corp weapons are at fault. Watch over the Road for the other caravans.",
+
+    requiredMissionWins: 6,
+  },
+
+  "Mission 7": {
+    Characters: ["zuark"],
+    Title: "Protect Caravans 3",
+    Message:
+      "Zu'Ark has protected a caravan on a different path, she said vortex are heading this way. Prepare for a battle.",
+
+    requiredMissionWins: 7,
+  },
+
+  "Mission 8": {
+    Characters: ["echo", "zuark"],
+    Title: "Protect Caravans 4",
+    Message:
+      "You succesfully take down the Vortex Squad. ECHO has collected more information from their damaged tech. Take it back to the City.",
+
+    requiredMissionWins: 8,
+  },
+
+  "Mission 9": {
+    Characters: ["echo", "veran", "zuark"],
+    Title: "Protect Caravans 5",
+    Message:
+      "The City learns that the trade routes are compromised by hidden Vortex Trackers. with the recovered information, they have the locations. Lead a warrior group to disable them and secure the trade routes.",
+
+    requiredMissionWins: 9,
+  },
+
+  "Mission 10": {
+    Characters: ["echo"],
+    Title: "Free Oases 1",
+    Message:
+      "Jahntow and Zu'ark liberate captured oases, which are crucial water sources for the Xictlians.",
+
+    requiredMissionWins: 10,
+  },
+
+  "Mission 11": {
+    Characters: ["echo", "zuark"],
+    Title: "Free Oases 2",
+    Message:
+      "Jahntow and Zu'ark liberate captured oases, which are crucial water sources for the Xictlians.",
+
+    requiredMissionWins: 11,
+  },
+
+  "Mission 12": {
+    Characters: ["echo", "zuark"],
+    Title: "Free Oases 3",
+    Message:
+      "Jahntow and Zu'ark liberate captured oases, which are crucial water sources for the Xictlians.",
+
+    requiredMissionWins: 12,
+  },
+
+  "Mission 13": {
+    Characters: ["echo", "zuark", "veran"],
+    Title: "Free Oases 4",
+    Message:
+      "Jahntow and Zu'ark liberate captured oases, which are crucial water sources for the Xictlians.",
+
+    requiredMissionWins: 13,
+  },
+  "Mission 14": {
+    Characters: ["echo", "zuark", "veran"],
+    Title: "Free Oases 5",
+    Message:
+      "Jahntow and Zu'ark liberate captured oases, which are crucial water sources for the Xictlians.",
+
+    requiredMissionWins: 14,
   },
 };
 const charactersImages = {
